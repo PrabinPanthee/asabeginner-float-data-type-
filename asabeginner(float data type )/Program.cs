f72c2c9ded5a50ac3130df9a2b005e0e6da4b3a1
iampr <iampr@PRABIN>
2023-01-03 13:49:12 +0545

    Add project files.

---
+++ asabeginner(float data type )/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float f = 122525.01f;
+        Console.WriteLine(f);
+        double d = 3.555555555555;
+        Console.WriteLine(d);
+       
+    }
+}
